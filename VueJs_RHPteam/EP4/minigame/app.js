3ba67d68c0b3d306e230651a683fc9f8b65d186a
unknown <vanhoan98.hp@gmail.com>
2022-10-19 23:24:56 +0700

    TramCamLuon

--- VueJs_RHPteam/EP4/minigame/app.js
+++ VueJs_RHPteam/EP4/minigame/app.js
@@ -1,10 +1,100 @@
 new Vue(
     {
-        el:'#app',
-        data:{
-            playerhealth: 100,
-            monsterhealth: 100,
+        el: '#app',
+        data: {
+            playerHealth: 100,
+            monsterHealth: 100,
             gameIsRunning: false
-        }
+        },
+        methods: {
+            startNewgame()
+            {
+                this.gameIsRunning = true;
+                this.playerHealth = 100;
+                this.monsterHealth = 100;
+            },
+            attack()
+            {
+                if (this.checkPlayerOption())
+                {
+                    return;
+                    }
+                //monster
+               
+                this.monsterHealth -= this.inputDamage(4,10);
+                //Player
+                this.monsterAttacks();
+            },
+            specialattack()
+            {
+
+                if (this.checkPlayerOption())
+                {
+                    return;
+                    }
+                //monster
+               
+                this.monsterHealth -= this.inputDamage(10,20);
+                //Player
+                this.monsterAttacks();
+            },
+            heal()
+            {
+                if (this.playerHealth > 70) {
+                    return false;
+                }
+                else if (this.playerHealth <= 60) {
+                    this.playerHealth += 10;
+                }
+                else
+                {
+                    this.playerHealth = 70;
+                    }
+            },
+            
+            giveup()
+            {
+                this.gameIsRunning = false;
+                alert("You loss");
+            },
+            monsterAttacks()
+            {
+                this.playerHealth -= this.inputDamage(5, 12);
+                this.checkPlayerOption();
+            },
+            inputDamage(minDamage, maxDamage)
+            {
+                return Math.max(Math.floor(Math.random() * maxDamage) + 1, minDamage);
+            },
+            checkPlayerOption()
+            {
+                if (this.monsterHealth <= 0)
+                {
+                    if (confirm("You won! New game"))
+                    {
+                        this.startNewgame();    
+                    }
+                    else
+                    {
+                        this.gameIsRunning = false;                       
+                    }
+                    return true;
+                } else if(this.playerHealth <= 0)
+                {
+                    
+                    if (confirm("You Lost! New game"))
+                    {
+                        this.startNewgame();
+                    }
+                    else
+                    {
+                        this.gameIsRunning = false;                       
+                    }
+                    return true;
+                }
+                return ;
+                
+            }
+        },
     }
-)
+);
